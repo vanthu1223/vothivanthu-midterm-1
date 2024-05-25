d88f397a4bb72d9f4b6538bae03ff3e5e07f10ce
thu <thu.vo25@student.passerellesnumeriques.org>
2024-05-25 11:03:41 +0700

    finish code

--- exercise-4/src/components/Place.jsx
+++ exercise-4/src/components/Place.jsx
@@ -2,7 +2,7 @@ export default function Place({place}) {
   return (
     <li key="{place.id}" className="place-item">
       <button>
-        <img src="{place.image.src}" alt="{place.image.src}" />
+        <img src={place.image.src} alt={place.image.alt} />
         <h3>{place.title}</h3>
       </button>
     </li>
