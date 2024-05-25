9b9247aa7787cf93310de4ec2f8aefbb8b0cf453
thu <thu.vo25@student.passerellesnumeriques.org>
2024-05-25 09:56:03 +0700

    finish exercise-3

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -18,7 +18,15 @@ function App() {
     <div id="app">
       <h1>PNV Team !!</h1>
       <p>Here are some PNV trainers and educators, do you know them?</p>
-      <User /> {/* How to loop on ALL_PNV_TEACHERS list ? */}
+      {ALL_PNV_TEACHERS.map((teacher,index) => {
+        <User 
+          key={index}
+          firstName ={teacher.firstName}
+          lastName ={teacher.lastName}
+          title = {teacher.title}
+        />
+      })}
+                   {/* How to loop on ALL_PNV_TEACHERS list ? */}
     </div>
   );
 }
